a91c3b86c123effee6b2017fbe6f89b8f1ae3109
robertsolorzano <robertsolorzano510@gmail.com>
2025-02-01 14:01:53 -0800

    Created basic user selections
    - made gitignore

--- ffmpeg_webm_to_mov.sh
+++ ffmpeg_webm_to_mov.sh
@@ -3,6 +3,21 @@
 # Script to ask use for file name of video to be converted (webm)
 # And name of the output (mov)
 
+echo 'Choose conversion type:'
+
+select choice in 'MP4' 'MP4-Lossless' 'MOV'; do
+	if [[ $choice == 'MP4' ]]; then
+		selected='libx264 -crf 18 -preset slow -c:a aac'
+	elif [[ $choice == 'MP4-Lossless' ]]; then
+		selected='libx264 -crf 0 -preset slow -c:a copy'
+	elif [[ $choice == 'MOV' ]]; then
+		selected='prores -profile:v 3 -c:a copy'
+	fi
+	break
+done
+
+echo You chose $choice
+
 echo 'Please specify video file to be converted (include file type)'
 
 read input
@@ -13,5 +28,5 @@ read output
 
 echo running script
 
-ffmpeg -i $input -c:v prores -profile:v 3 -c:a copy $output
+ffmpeg -i $input -c:v $selected $output
 
